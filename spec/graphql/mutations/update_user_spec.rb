7dab3a3c49fdda9224ef8242fce6038139a213e9
sumit vishvakarma <svishvakarma@bestpeers.com>
2023-07-27 20:05:51 +0530

    rspec for update user

---
+++ spec/graphql/mutations/update_user_spec.rb
@@ -0,0 +1,18 @@
+require 'rails_helper'
+
+RSpec.describe Mutations::UpdateUser do
+ let(:username) { 'svishwakarma' }
+ let(:repositories_count) { 10 }
+
+  it 'creates a new user with valid credentials' do
+    context = { object: nil, context: { current_user: nil }, field: nil }
+    result = Mutations::UpdateUser.new(object: nil, field: nil, context: context).resolve(username: username, repositories_count: repositories_count)
+    expect(result[:user].username).to eq(username)
+    expect(result[:user].repositories_count).to eq(repositories_count)
+    expect(result[:errors]).to be_empty
+  end
+end
+
+
+
+
